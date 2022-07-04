2afa431753f098f5fb1f535e1089a40176fb1f15
0/0 <zero@imaskeleton.me>
2022-07-03 23:34:02 -0600

    register and add logging

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,27 @@ use std::path::Path;
 use tokio::net::unix::SocketAddr;
 use tokio::net::UnixStream;
 
+#[macro_use]
+extern crate tracing;
+
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
+    info!("loading models");
     stts_speech_to_text::load_models(Path::new(
         &std::env::args()
             .nth(1)
             .expect("first argument should be path to model directory"),
     ));
+    info!("loaded models");
 
+    info!("opening Unix Domain Socket");
     let socket =
         tokio::net::UnixListener::bind("/tmp/stts.sock").expect("failed to bind to socket");
+    info!("opened Unix Domain Socket");
 
+    info!("polling for connections");
     loop {
         // accept connections and spawn a task for each one
         // or if ctrl+c is received, break the loop
