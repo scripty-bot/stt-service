67631bbbfd16f3b3db0dc33f39a2f1bf0a0a30fb
Zero <zero@imaskeleton.me>
2023-05-06 10:16:17 -0600

    update whisper-rs to harness CUDA/OpenCL acceleration

--- stts_speech_to_text/src/lib.rs
+++ stts_speech_to_text/src/lib.rs
@@ -1,17 +1,16 @@
 use once_cell::sync::OnceCell;
 use std::fmt::Write;
 use std::sync::atomic::{AtomicUsize, Ordering};
-use tokio::sync::Mutex;
+use std::sync::Arc;
+use tokio::sync::{Mutex, Semaphore};
 
 pub use whisper_rs::*;
 
 #[macro_use]
 extern crate tracing;
 
-pub static MODELS: OnceCell<(
-    flume::Sender<WhisperContext>,
-    flume::Receiver<WhisperContext>,
-)> = OnceCell::new();
+pub static MODEL: OnceCell<WhisperContext> = OnceCell::new();
+pub static MAX_CONCURRENCY: OnceCell<Arc<Semaphore>> = OnceCell::new();
 pub static WAITING_COUNT: AtomicUsize = AtomicUsize::new(0);
 
 pub fn get_load() -> usize {
@@ -19,58 +18,31 @@ pub fn get_load() -> usize {
 }
 
 pub fn load_models(model_path: &str, instances: usize) {
-    info!("attempting to load model once");
+    info!("attempting to load model");
     let model = WhisperContext::new(model_path).expect("failed to load model");
-    info!("loaded model once, now loading {} instances", instances);
-    let (sender, receiver) = flume::unbounded();
-    sender.send(model).expect("failed to send model to channel");
-    for _ in 1..instances {
-        sender
-            .send(WhisperContext::new(model_path).expect("failed to load model"))
-            .expect("failed to send model to channel");
-    }
-    info!("loaded {} instances of model", instances);
-    MODELS
-        .set((sender, receiver))
-        .expect("failed to set models");
+    MODEL.set(model).expect("failed to set models");
+    info!("loaded model");
+
+    info!("max concurrency: {}", instances);
+    MAX_CONCURRENCY
+        .set(Arc::new(Semaphore::new(instances)))
+        .expect("failed to set max concurrency");
 }
 
-fn get_new_model() -> Option<WhisperContext> {
-    let models = MODELS.get().expect("models not initialized");
-    // try to fetch a model from the pool
-    WAITING_COUNT.fetch_add(1, Ordering::SeqCst);
-    let maybe_ctx = models.1.recv();
-    WAITING_COUNT.fetch_sub(1, Ordering::SeqCst);
-
+fn get_new_model() -> Option<WhisperState<'static>> {
     // if we got a model, return it
     // on error, log it and return None
-    match maybe_ctx {
-        Ok(ctx) => Some(ctx),
-        Err(e) => {
-            error!("failed to get model from pool (should never happen): {}", e);
+    match MODEL.get() {
+        Some(ctx) => ctx.create_state().ok(),
+        None => {
+            error!("models not set up yet: check that load_models was called");
             None
         }
     }
 }
 
-async fn reap_model(model: WhisperContext) {
-    trace!("reaping model");
-    let models = MODELS.get().expect("models not initialized");
-    // try to send the model back to the pool
-    // on error, log it and drop the model
-    match models.0.send_async(model).await {
-        Ok(_) => trace!("reaped model"),
-        Err(e) => error!(
-            "failed to send model back to pool (should never happen): {}",
-            e
-        ),
-    }
-}
-
 fn create_model_params(language: &str) -> FullParams {
-    let mut params = FullParams::new(SamplingStrategy::Greedy {
-        best_of: 1,
-    });
+    let mut params = FullParams::new(SamplingStrategy::Greedy { best_of: 1 });
     params.set_n_threads(1);
     params.set_print_progress(false);
     params.set_print_realtime(false);
@@ -91,7 +63,7 @@ impl SttStreamingState {
     pub fn new(language: String) -> Self {
         Self {
             stream_data: Mutex::new(Vec::new()),
-            language
+            language,
         }
     }
 
@@ -101,13 +73,24 @@ impl SttStreamingState {
     }
 
     pub async fn finish_stream(self, verbose: bool) -> Result<String, WhisperError> {
-        let Self { stream_data, language } = self;
+        let Self {
+            stream_data,
+            language,
+        } = self;
 
         // we own the stream data now, so we can drop the lock
         let audio_data = stream_data.into_inner();
 
+        let permit = MAX_CONCURRENCY
+            .get()
+            .expect("max concurrency not set")
+            .clone()
+            .acquire_owned()
+            .await
+            .expect("semaphore should not be closed");
+
         // use tokio blocking threads to process the audio
-        let (model, res) = tokio::task::spawn_blocking(move || {
+        let (state, res) = tokio::task::spawn_blocking(move || {
             let audio_data = audio_data;
             // process to mono 16KHz f32
             // the input is mono 16KHz i16
@@ -117,13 +100,16 @@ impl SttStreamingState {
             let params = create_model_params(&language);
 
             // get a model from the pool
-            let mut model = get_new_model().expect("failed to get model from pool");
+            let mut state = get_new_model().expect("failed to get model from pool");
 
             // run the model
-            let res = model.full(params, &audio_data);
+            let res = state.full(params, &audio_data);
+
+            // for all intents and purposes, we are done with the model
+            drop(permit);
 
             // return the model and the result to the async context
-            (model, res)
+            (state, res)
         })
         .await
         .expect("model thread panicked (should never happen)");
@@ -131,39 +117,32 @@ impl SttStreamingState {
         // check if the model failed
         if let Err(e) = res {
             error!("model failed: {:?}", e);
-            // reap the model!
-            reap_model(model).await;
             return Err(e);
         }
 
         // get the result
-        let num_segments = model.full_n_segments();
+        let num_segments = state.full_n_segments()?;
         // average english word length is 5.1 characters which we round up to 6
         let mut segments = String::with_capacity(6 * num_segments as usize);
         for i in 0..num_segments {
-            match (model.full_get_segment_text(i), verbose) {
+            match (state.full_get_segment_text(i), verbose) {
                 (Ok(s), false) => {
                     segments.push_str(&s);
                     segments.push('\n');
                 }
                 (Ok(s), true) => {
                     // also add the start and end time
-                    let start = model.full_get_segment_t0(i);
-                    let end = model.full_get_segment_t1(i);
+                    let start = state.full_get_segment_t0(i)?;
+                    let end = state.full_get_segment_t1(i)?;
                     writeln!(segments, "[{} - {}]: {}", start, end, s)
                         .expect("failed to write segment");
                 }
                 (Err(e), _) => {
                     error!("failed to get segment text: {:?}", e);
-                    // reap the model!
-                    reap_model(model).await;
                     return Err(e);
                 }
             };
         }
-
-        // reap the model!
-        reap_model(model).await;
 
         // return the result
         Ok(segments)
